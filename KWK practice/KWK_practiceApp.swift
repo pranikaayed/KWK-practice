90a69a968237c83de600b1d03deaf0b7206e5dd8
rkyed809 <ravi.yedati@marriott.com>
2024-06-18 12:58:12 -0400

    Initial Commit

---
+++ KWK practice/KWK_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  KWK_practiceApp.swift
+//  KWK practice
+//
+//  Created by Ravi Yedati on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct KWK_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
